a98338d7c681313e2fc7f90e83a0cb3d17645edd
EllieSheppard2 <elliesheppard213@gmail.com>
2026-02-12 10:54:23 -0500

    look at rating distribution

--- word_db_creation.py
+++ word_db_creation.py
@@ -1,4 +1,10 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 montreal = pd.read_csv('listings.csv')
+print(montreal.head(10))
+montreal['review_scores_rating'].plot(kind='hist', bins = 50)
+plt.xticks(np.arange(0, 5, .125), fontsize=3)
+plt.show()
+#choose "highly rated" threshold at 4.75
