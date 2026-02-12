67027eb295388dcc5518f1716c332cfe31b6cea6
EllieSheppard2 <elliesheppard213@gmail.com>
2026-02-12 10:56:30 -0500

    add todos

--- word_db_creation.py
+++ word_db_creation.py
@@ -8,3 +8,6 @@ montreal['review_scores_rating'].plot(kind='hist', bins = 50)
 plt.xticks(np.arange(0, 5, .125), fontsize=3)
 plt.show()
 #choose "highly rated" threshold at 4.75
+#TODO: create db of highly rated, and one of low rated listings, only include "word" cols
+
+#TODO: created a dataframe of words in each column for both. words and counts
