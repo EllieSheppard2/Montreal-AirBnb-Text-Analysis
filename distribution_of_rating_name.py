ed44d5c7a5f8c5a093c82a87badca3a84fb7c284
EllieSheppard2 <elliesheppard213@gmail.com>
2026-03-04 21:00:15 -0500

    notes, finding words with high discrepancy in mean ratings for has/has not

--- distribution_of_rating_name.py
+++ distribution_of_rating_name.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from word_db_creation import load_data
+import re
+from collections import Counter
 
 montreal = load_data()
 
@@ -10,7 +12,7 @@ montreal['search_text'] = (
     montreal['name'] + " " + montreal['description']
 ).str.lower()
 
-word_of_interest = "bright" #change to whatever word we are looking for
+word_of_interest = ("fully") #change to whatever word we are looking for
 
 montreal['has_word'] = montreal['search_text'].str.contains(rf'\b{word_of_interest}\b')
 montreal_clean = montreal.dropna(subset=['review_scores_rating'])
@@ -22,14 +24,65 @@ plt.figure()
 plt.hist(has_not['review_scores_rating'], bins=30, alpha=0.25, color="purple", label='Does Not Have {word_of_interest}'.format(word_of_interest=word_of_interest))
 plt.hist(has['review_scores_rating'], bins=30, alpha=0.9, color="pink", label='Has {word_of_interest}'.format(word_of_interest=word_of_interest))
 plt.legend()
-plt.show()
+#plt.show()
+
+#Just from looking at some suspect words graphs,
 
 #not relevant:
-#downtown, cozy, modern
+#downtown, cozy, modern, heart
 
 #relevant:
-#mtl? bright?
+#mtl? bright? luxury
 
 #overall, outliers in low ranking seems to be only listings without most common words. maybe investigate having a common keyword as a whole?(not specifics)
 
-#TODO: run multiple linear regression on 5 most promising words?
+#Find the top five words with the biggest difference in rating of listings that have them in description and those that do not
+text = " ".join(montreal_clean['description'])
+
+words = re.findall(r'\b[a-z]+\b', text)
+word_counts = Counter(words)
+candidate_words = [w for w, c in word_counts.items() if c >= 30]
+
+results = []
+
+#This part takes a LONG time to run. To note: making len has/has not and candidate word count too high will give words with
+#virtually no difference in rating as it will just return words in almost all listings. However, too low and it will take even longer to run.
+#Also, running with len has/has not as 1 and candidate words as 2 yields results where single listings with low ratings get factored in.
+#Ideal seems to be len as 10, candidate as 30.
+for word in candidate_words:
+
+    has_word = montreal_clean['description'].str.contains(rf'\b{word}\b')
+
+    has = montreal_clean[has_word]
+    has_not = montreal_clean[~has_word]
+
+    if len(has) < 10 or len(has_not) < 10:
+        continue
+
+    mean_has = has['review_scores_rating'].mean()
+    mean_not = has_not['review_scores_rating'].mean()
+
+    diff = mean_has - mean_not
+
+    results.append({
+        'word': word,
+        'mean_has': mean_has,
+        'mean_not': mean_not,
+        'diff': diff,
+        'abs_diff': abs(diff),
+        'count_has': len(has)
+    })
+
+results_df = pd.DataFrame(results)
+
+top5 = results_df.sort_values('abs_diff', ascending=False).head(5)
+
+print(top5)
+
+#Pasting output as it takes long to run
+#               word  mean_has  mean_not      diff  abs_diff  count_has
+#931           value  4.418710  4.724992 -0.306282  0.306282         31
+#1040         hostel  4.440339  4.725956 -0.285617  0.285617         59
+#1041  establishment  4.456364  4.724909 -0.268546  0.268546         33
+#1068         onsite  4.989688  4.721500  0.268187  0.268187         64
+#304        tourists  4.462250  4.725126 -0.262876  0.262876         40
