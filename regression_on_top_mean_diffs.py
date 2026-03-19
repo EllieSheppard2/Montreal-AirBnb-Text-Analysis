db00d6db697bae44bedb639e558474e4f83f8cbc
EllieSheppard2 <elliesheppard213@gmail.com>
2026-03-19 10:25:18 -0400

    adding more variables

--- regression_on_top_mean_diffs.py
+++ regression_on_top_mean_diffs.py
@@ -6,8 +6,8 @@ montreal = load_data()
 montreal = montreal.dropna(subset=['review_scores_rating']).copy()
 montreal['description'] = montreal['description'].fillna('').str.lower()
 
-words_to_use = ['value', 'hostel', 'establishment', 'tourists', 'onsite', 'resort', 'kitchenette', 'doorman', 'homey', 'university',
-                'student', 'jacuzzi', 'enclave', 'hospitals']
+words_to_use = ['value', 'hostel', 'tourists', 'kitchenette', 'university',
+                'student', 'enclave', 'hospitals']
 
 for word in words_to_use:
     montreal[f'has_{word}'] = montreal['description'].str.contains(rf'\b{word}\b').astype(int)
@@ -18,8 +18,6 @@ y = montreal['review_scores_rating']
 model = sm.OLS(y, X).fit()
 print(model.summary())
 
-#may consider adding more words to this model? establishment becomes non significant
-
 #added resort, kitchenette, doorman to value, hostel, establishment, onsite, tourists. now onsite is insignificant as well as kitchenette, doorman, establishment, resort
 
 #re-running without those. other variables stay significant
@@ -27,3 +25,7 @@ print(model.summary())
 #so, hostel value and tourist key words in description have significance in rating. however, they only explain 0.008 of the variation in scores.
 
 #actually going with printing top 30 out of find_discrepancies, then doing regression with those that have gaps above .20 and look resonable, ie. don't include "st"
+
+#now, remove non-significant at 0.05 level
+
+#final model: value, hostel, tourists, kitchenette, univeristy, student, enclave, hospitals
